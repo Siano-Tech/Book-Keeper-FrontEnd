13d6a6f4527c983855342337cf5ffdb1eaf264da
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-10-17 19:36:51 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 // Default Axis Endpoint for all the requests
-axios.defaults.baseURL = 'http://192.168.0.130:5000';
-// axios.defaults.baseURL = 'https://book-keeper-backend.vercel.app';
+// axios.defaults.baseURL = 'http://192.168.0.130:5000';
+axios.defaults.baseURL = 'https://book-keeper-backend.vercel.app';
