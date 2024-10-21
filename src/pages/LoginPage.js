86363678f5ff110f93804be765465f95f82ae161
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-10-21 16:23:43 +0530

    Update LoginPage.js to make input take phone & email

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -83,16 +83,16 @@ const LoginPage = () => {
           </div>}
           <div>
             <label htmlFor="tel" className="block text-sm font-medium leading-6 text-gray-900">
-              Phone No
+            {(!hasAccount ? 'Phone No' : 'Phone No or Email')}
             </label>
             <div className="mt-2">
               <input
                 id="phoneNo"
                 name="tel"
-                type="tel"
-                autoComplete="tel"
+                type="text"
+                autoComplete="text"
                 value={phoneNo}
-                maxLength={10}
+                // maxLength={10}
                 onChange={(e) => setPhoneNo(e.target.value)}
                 required
                 className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
